a1a54842bb6610d2fbe4cbb29a3481268678c9e8
André Vitor <salesdias1207@gmail.com>
2026-09-23 21:45:52 -0300

    Enhance Categoria model with Lombok annotations for boilerplate reduction and add a one-to-many relationship with Curso.

--- projeto_escola/src/main/java/com/example/projeto_escola/models/Categoria.java
+++ projeto_escola/src/main/java/com/example/projeto_escola/models/Categoria.java
@@ -2,15 +2,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Data // esse é o getter e setter da classe, que é usado para get e set os atributos da classe
+@Builder // esse é o construtor da classe, que é usado para criar uma nova instância da classe
+@AllArgsConstructor // esse é o construtor com todos os argumentos, que é usado para criar uma nova instância da classe com todos os argumentos
+@NoArgsConstructor // esse é o construtor vazio, que é usado para criar uma nova instância da classe sem argumentos
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @OneToMany(mappedBy = "categoria")
+    private List<Curso> cursos;
 }
