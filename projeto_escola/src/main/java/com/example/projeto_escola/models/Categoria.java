e991233d9196f9e10704f0ec2505372a5718447e
André Vitor <salesdias1207@gmail.com>
2026-09-23 21:42:33 -0300

    Remove all demo project files including Maven configuration, application code, and test cases.

---
+++ projeto_escola/src/main/java/com/example/projeto_escola/models/Categoria.java
@@ -0,0 +1,16 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+}
--- projeto_escola/src/test/java/com/example/projeto_escola/ProjetoEscolaApplicationTests.java
+++ projeto_escola/src/test/java/com/example/projeto_escola/ProjetoEscolaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.projeto_escola;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProjetoEscolaApplicationTests {
 
 	@Test
 	void contextLoads() {
